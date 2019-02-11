7576b6757a462bda28af43db0ba54c47ef0d6bbf
james_r_lewis@hotmail.com <james_r_lewis@hotmail.com>
2019-02-11 12:03:31 +0000

    monday morning backup

--- phoenix-DQN.py
+++ phoenix-DQN.py
@@ -9,10 +9,11 @@ from skimage.color import rgb2gray
 from collections import deque
 
 
+stack_size=4
 
 
 def preprocess_frame(state):
-    gray=rgb2gray(observation)
+    gray=rgb2gray(state)
     #cropped_frame=gray[8:-12,4:-12]
     cropped_frame=gray[22:-28,:]
     downscale=block_reduce(cropped_frame,block_size=(4,4),func=np.mean)
@@ -32,54 +33,57 @@ def stack_frames(stacked_frames,state,is_new_episode):
     else:
         stacked_frames.append(frame)
 
-    plt.imshow(frame,interpolation='nearest')
-    plt.show()
+    #plt.imshow(frame,interpolation='nearest')
+    #plt.show()
     #print(stacked_frames)
     return stacked_frames
 
+def test_environment(number_of_episodes):
+    env=gym.make('Phoenix-v0')
 
+    #leads to  action every 60/4 of a second
+    stacked_frames=deque([np.zeros((40,40),dtype=np.int) for i in range (stack_size)],maxlen=4)
 
-env=gym.make('Phoenix-v0')
-
-bins=np.zeros(9)
-bins=np.linspace(-1.3,0.5,9)
-obs0=np.zeros(10)
-averageScores=[]
-
-#leads to  action every 60/4 of a second
-stack_size=4
-
-stacked_frames=deque([np.zeros((40,40),dtype=np.int) for i in range (stack_size)],maxlen=4)
-
-for i_episode in range(10):
-    observation=env.reset()
-    totalRew=0
-    is_new_episode=True
-    for t in range(100000):
-        #env.render()
-        action=env.action_space.sample()
-        observation,reward,done,info=env.step(action)
-       
-        stacked_frames=stack_frames(stacked_frames,observation,is_new_episode)
-        is_new_episode=False
-        
-        #plt.imshow(downscale,interpolation='nearest')
-        #plt.show()
-        observation=np.around(observation,2)
-        totalRew+=reward
-        index=np.digitize(observation[5],bins)
-        
-        
-        if done:
+    for i_episode in range(number_of_episodes):
+        observation=env.reset()
+        totalRew=0
+        is_new_episode=True
+        for t in range(100000):
+            #env.render()
+            action=env.action_space.sample()
+            observation,reward,done,info=env.step(action)
            
-            #print("Episode finished after {} timesteps".format(t+1))
-           break
-    print(i_episode,totalRew)
-    #averageScores.append(totalRew/500)
+            stacked_frames=stack_frames(stacked_frames,observation,is_new_episode)
+            is_new_episode=False
+            
+            #plt.imshow(downscale,interpolation='nearest')
+            #plt.show()
+            observation=np.around(observation,2)
+            totalRew+=reward
+            
+            
+            if done:
+                if(totalRew>4000): 
+                    plt.imshow(stacked_frames.pop(),interpolation='nearest')    
+                    
+                    plt.show()
+                    plt.imshow(stacked_frames.pop(),interpolation='nearest')    
+                    plt.show()
+                    plt.imshow(stacked_frames.pop(),interpolation='nearest')    
+                    plt.show()
+                    plt.imshow(stacked_frames.pop(),interpolation='nearest')    
+                    plt.show()
+                    #print("Episode finished after {} timesteps".format(t+1))
+                break
+        print(i_episode,totalRew)
+        #averageScores.append(totalRew/500)
+    
+    env.close();
 
 
-#print(np.sum(averageScores)/len(averageScores))
+######################################################
+################### Actual Code running ##############
+######################################################
 
+test_environment(500)
 
-
-env.close();

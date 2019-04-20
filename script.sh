40e682f752e8a0a0486ed8ca5395e4ad725d783a
james_r_lewis@hotmail.com <james_r_lewis@hotmail.com>
2019-04-20 13:38:32 +0100

    scripts

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@ while inotifywait -q -e create models/ >/dev/null; do
     
    	sleep 10 
 	echo $counter
-    	counter=$((counter+1))
     	cp -rf models/ m/models$counter
-        # do whatever else you need to do
+        
+    	counter=$((counter+1))
+	# do whatever else you need to do
 done
